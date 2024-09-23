a7694f35a2a3312483266b5f68092f90eca79c42
SkyB_13 <130376809+SkyB13@users.noreply.github.com>
2024-09-23 11:39:02 +0300

    Update Main.kt
    Вывод данных в main

--- Patterns/src/Main.kt
+++ Patterns/src/Main.kt
@@ -2,5 +2,12 @@ import java.time.LocalDate
 import java.util.*
 import java.util.regex.Pattern
 
+fun main() {
+    val student1 = Student(UUID.randomUUID(), LocalDate.of(2000, 1, 1), "Иванов", "Иван", "Петрович", "+79991234567")
+    val student2 = Student(UUID.randomUUID(), LocalDate.of(2001, 12, 31), "Сидоров", "Александр", phone = "+79876543210", telegram = "@sidorov")
+
+    println(student1.toString())
+    println(student2.toString())
+}
 
 
