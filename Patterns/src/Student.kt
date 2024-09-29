59efea4bad5c9d17ad9f90663d2b9df2a2341174
SkyB_13 <130376809+SkyB13@users.noreply.github.com>
2024-09-29 20:13:31 +0300

    Update Student.kt
    Добавлены валидации

--- Patterns/src/Student.kt
+++ Patterns/src/Student.kt
@@ -66,4 +66,40 @@ class Student(
              phone=${phone ?: ""}, telegram=${telegram ?: ""}, email=${email ?: ""}, git=${git ?: ""})
         """.trimIndent()
     }
+
+    companion object {
+        fun isValidPhone(phone: String?): Boolean {
+            return phone == null || Pattern.matches("\\+\\d{11}", phone)
+        }
+
+        fun validateEmail(email: String?) {
+            if (!email.isNullOrEmpty() && !Pattern.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}", email)) {
+                throw IllegalArgumentException("Неверный формат email")
+            }
+        }
+
+        fun validateGit(git: String?) {
+            if (!git.isNullOrEmpty() && !Pattern.matches("https://github\\.com/[a-zA-Z0-9_-]+", git)) {
+                throw IllegalArgumentException("Неверный формат ссылки на GitHub")
+            }
+        }
+
+        fun validate(student: Student): Boolean {
+            return student.git != null || (student.phone != null && isValidPhone(student.phone)) ||
+                    (student.email != null && validateEmail(student.email) == Unit) ||
+                    (student.telegram != null)
+        }
+    }
+
+    fun setContacts(phone: String? = this.phone, telegram: String? = this.telegram,
+                    email: String? = this.email, git: String? = this.git) {
+        validatePhone(phone)
+        validateEmail(email)
+        validateGit(git)
+
+        this.phone = phone
+        this.telegram = telegram
+        this.email = email
+        this.git = git
+    }
 }
