d1801b859a4206d34754214875f9fac9ede63d40
SkyB_13 <130376809+SkyB13@users.noreply.github.com>
2024-10-06 22:38:23 +0300

    Update Person.kt
    Задание 2. Добавление исключения для невозможности парсинга

--- Patterns/Patterns_lab2/src/main/kotlin/Person.kt
+++ Patterns/Patterns_lab2/src/main/kotlin/Person.kt
@@ -8,18 +8,41 @@ class Person(
     val birthDate: LocalDate
 ) {
     companion object {
-        // Новый конструктор для парсинга строки
         fun parseFromString(input: String): Person {
-            val (name, ageStr, birthDateStr) = input.split(',')
-            val age = ageStr.toInt()
-            val birthDate = LocalDate.parse(birthDateStr)
-            return Person(name, age, birthDate)
+            try {
+                val parts = input.split(',')
+
+                if (parts.size != 3) {
+                    throw ParsingException("Неверный формат входной строки")
+                }
+
+                val name = parts[0].trim()
+                val ageStr = parts[1].trim()
+                val birthDateStr = parts[2].trim()
+
+                val age = ageStr.toIntOrNull() ?: throw ParsingException("Возраст должен быть числом")
+
+                val birthDate = LocalDate.parse(birthDateStr)
+
+                if (age < 0 || age > 150) {
+                    throw ValidationException("Возраст должен быть от 0 до 150 лет")
+                }
+
+                return Person(name, age, birthDate)
+            } catch (e: Exception) {
+                throw ParsingException("Ошибка при парсинге строки")
+            }
         }
     }
 
+
     override fun toString(): String {
-        return "Person(name='$name', age=$age, birthDate=$birthDate)"
+        return "Person (name: '$name', age: $age, birthDate: $birthDate)"
     }
+
+
 }
 
+class ParsingException(message: String) : Exception(message)
+class ValidationException(message: String) : Exception(message)
 
