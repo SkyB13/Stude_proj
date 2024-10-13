f8b579f8c7e24d69fa4b67b9037d26ca86f2f244
SkyB_13 <130376809+SkyB13@users.noreply.github.com>
2024-10-13 22:45:29 +0300

    Update Main.kt
    Проверка вывода для Задание №4

--- Patterns/Patterns_lab2/src/main/kotlin/Main.kt
+++ Patterns/Patterns_lab2/src/main/kotlin/Main.kt
@@ -2,13 +2,27 @@ import java.time.LocalDate
 import java.util.*
 
 fun main() {
-    // Тестирование нового конструктора c разделением данных (getInfo, задание №3)
+    // Тестирование нового конструктора c разделением данных
     val person = Person.parseFromString("John Doe, 30, 1990-01-01")
     println(person.getInfo())
 
     // Тестирование стандартного конструктора
     val personWithParams = Person("Jane Smith", 25, LocalDate.of(1995, 6, 15))
     println(personWithParams)
+
+
+
+    //вывод для проверки класса Student_short
+    println("Тест. Задание №4")
+    val student = Student(1, "Doe", "J. D.", "john.doe@example.com")
+    val studentShort1 = Student_short(student)
+    println(studentShort1)
+
+    // Тестируем второй конструктор (с ID и строкой)
+    val studentShort2 = Student_short(2, "Smith, A. S., alice.smith@example.com")
+    println(studentShort2)
+
+
 
     try {
         val person = Person.parseFromString("John Doe, 30, 1990-01-01")
