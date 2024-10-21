9cc497c363695a7f7f87d4be53895e5e79d924e8
SkyB_13 <130376809+SkyB13@users.noreply.github.com>
2024-10-21 13:20:00 +0300

    Update Main.kt

--- Patterns/src/Main.kt
+++ Patterns/src/Main.kt
@@ -2,12 +2,25 @@ import java.time.LocalDate
 import java.util.*
 import java.util.regex.Pattern
 
+
 fun main() {
-    val student1 = Student(UUID.randomUUID(), LocalDate.of(2000, 1, 1), "Иванов", "Иван", "Петрович", "+79991234567")
-    val student2 = Student(UUID.randomUUID(), LocalDate.of(2001, 12, 31), "Сидоров", "Александр", phone = "+79876543210", telegram = "@sidorov")
+    val student = Student()
 
-    println(student1.toString())
-    println(student2.toString())
+    try {
+        // Устанавливаем данные через сеттеры
+        student.firstName = "Иван"
+        student.lastName = "Иванов"
+        student.middleName = "Петрович"
+        student.birthDate = LocalDate.of(2000, 1, 1)
+        student.telPhone = "+79991234567"    // Устанавливаем телефон через сеттер
+        //student.telegram = "@ivanov13"         // Устанавливаем Telegram через сеттер
+        student.email = "ivanov@mail.ru"     // Устанавливаем email через сеттер
+        student.git = "https://github.com/ivanov"  // Устанавливаем ссылку на Git через сеттер
+
+        // Выводим данные студента
+        println(student)
+
+    } catch (e: IllegalArgumentException) {
+        println("Ошибка: ${e.message}")
+    }
 }
-
-
