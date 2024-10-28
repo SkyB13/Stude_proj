c06aab8a15bcff4d26787449067c87b3682e8b8d
SkyB_13 <130376809+SkyB13@users.noreply.github.com>
2024-10-28 11:37:08 +0300

    Update Main.kt

--- Patterns/src/Main.kt
+++ Patterns/src/Main.kt
@@ -1,26 +1,16 @@
-import java.time.LocalDate
-import java.util.*
-import java.util.regex.Pattern
-
+import main.kotlin.Student
 
 fun main() {
-    val student = Student()
-
-    try {
-        // Устанавливаем данные через сеттеры
-        student.firstName = "Иван"
-        student.lastName = "Иванов"
-        student.middleName = "Петрович"
-        student.birthDate = LocalDate.of(2000, 1, 1)
-        student.telPhone = "+79991234567"    // Устанавливаем телефон через сеттер
-        //student.telegram = "@ivanov13"         // Устанавливаем Telegram через сеттер
-        student.email = "ivanov@mail.ru"     // Устанавливаем email через сеттер
-        student.git = "https://github.com/ivanov"  // Устанавливаем ссылку на Git через сеттер
-
-        // Выводим данные студента
-        println(student)
-
-    } catch (e: IllegalArgumentException) {
-        println("Ошибка: ${e.message}")
-    }
+    val student = Student(
+        "Иванов",
+        "Иван",
+        "Петрович",
+        "+799912цц34567",
+        "@ivanovan",
+        "ivanov@gmail.com",
+        "https://github.com/ivanov"
+    )
+    println(student.toString())
+    println("Проверка валидации:")
+    println("Запись 1: ${student.validate()}")
 }
