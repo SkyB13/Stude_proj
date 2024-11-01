1176589cdd43e572ec891a29c6585b819697a937
SkyB_13 <130376809+SkyB13@users.noreply.github.com>
2024-11-01 16:33:15 +0300

    Update Lab_2.py

--- OpenCV_labs/Lab_2.py
+++ OpenCV_labs/Lab_2.py
@@ -5,19 +5,19 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = cap.read()
-    if not ret:
+    ret_frame, frame = cap.read()
+    if not ret_frame:
         break
 
     #Задание 1. Преобразование в HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     #Задание 2. Задаем цвет (красный) для HSV
-    lower_red = np.array([0, 70, 50])
-    upper_red = np.array([10, 255, 255])
+    left_red = np.array([0, 70, 50])
+    right_red = np.array([10, 255, 255])
 
     #Создание и применение маски для указанного цвета
-    mask = cv2.inRange(hsv, lower_red, upper_red)
+    mask = cv2.inRange(hsv_img, left_red, right_red)
     result = cv2.bitwise_and(frame, frame, mask=mask)
 
     #Задание 3. Морфологические преобразования
@@ -25,34 +25,28 @@ while True:
     opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=2)
     closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel, iterations=2)
 
-    #Задание 4. Вычисление моментов и поиск контуров
-    conts, _ = cv2.findContours(closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+    #Задание 4, задание 5. Вычисление моментов и поиск контуров
     # Создание копии исходного кадра
     drawFrame = frame.copy()
+    moment = cv2.moments(opening)
+    area = moment['m00']
 
-    for cont in conts:
-        moments = cv2.moments(cont)
-        area = moments['m00']
+    if (moment["m00"] != 0):
+        x_val = int(moment['m10'] / moment['m00'])
+        y_val = int(moment['m01'] / moment['m00'])
+        weight = int(np.sqrt(moment['m00']))
+        high = int(moment['m00']//weight)
+        cv2.rectangle(drawFrame, (x_val - weight//20, y_val - high//20), (x_val + weight//20, y_val + high//20), (0, 0, 255), 4)
+        cv2.putText(drawFrame, f'Area: {area}', (x_val, y_val-10), cv2.FONT_HERSHEY_COMPLEX, 0.9, (0, 0, 0), 2)
 
-        if area > 1000:  # Фильтрация по площади
-            x, y, w, h = cv2.boundingRect(cont)
-
-            # Построение контура (прямоугольника)
-            cv2.rectangle(drawFrame, (x, y), (x + w, y + h), (0, 0, 0), 2)
-
-            # Вывод получаемой площади
-            cv2.putText(drawFrame, f'Area: {int(area)}', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
-                        (0, 0, 0), 2)
-
-    #Вывод результатов (несколько окон)
-    cv2.imshow('Original', frame)
-    cv2.imshow('HSV', hsv)
-    cv2.imshow('Mask', mask)
-    cv2.imshow('Result', result)
-    cv2.imshow('Opening', opening)
-    cv2.imshow('Closing', closing)
-    cv2.imshow('Final Result', drawFrame)
+    #Вывод результатов (оригинал, перевод в HSV, маска, результат маски, открытие и закрытие (морфологическое преобразование))
+    cv2.imshow('Original img', frame)
+    cv2.imshow('HSV img', hsv_img)
+    cv2.imshow('Mask img', mask)
+    cv2.imshow('Result img', result)
+    cv2.imshow('Opening img', opening)
+    cv2.imshow('Closing img', closing)
+    cv2.imshow('Final img', drawFrame)
 
     #Ожидание закрытия (если нажата клавиша esc, программа завершает выполнение)
     key = cv2.waitKey(1) & 0xFF
